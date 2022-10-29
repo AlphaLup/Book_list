8c29fd2edb2e263562352775a877c14977a02eb0
Gustavo Amancio <gustavoamaf7@gmail.com>
2022-10-28 23:00:49 -0300

    Initial commit

---
+++ app/src/main/java/prototype/booklist/InfoLivro.java
@@ -0,0 +1,6 @@
+package prototype.booklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InfoLivro extends AppCompatActivity {
+}
---
+++ app/src/main/java/prototype/booklist/MainAdapter.java
@@ -0,0 +1,24 @@
+package prototype.booklist;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
